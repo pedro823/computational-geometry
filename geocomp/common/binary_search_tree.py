4bd333e6b3b42cf46c561692aead1ec02ec0e82f
pedro823 <grey.pedrinho@gmail.com>
2018-10-08 19:33:29 -0300

    Extends point and segment class, fixes io.py, adds stub for BBST, adds point_visibility

---
+++ geocomp/common/binary_search_tree.py
@@ -0,0 +1,6 @@
+class BinarySearchTree:
+    ''' Implements a balanced binary search tree.
+        Elements inside it must be comparable.
+    '''
+    def __init__(self, compare_function=None):
+        pass
--- utils/embaralha.py
+++ utils/embaralha.py
@@ -3,10 +3,4 @@
 import random
 
 def embaralha (l):
-	for i in range (len (l) -1, -1, -1):
-		index = int (random.uniform (0, i+1))
-		aux = l[i]
-		l[i] = l[index]
-		l[index] = aux
-	
-	return l
+    return random.shuffle(l)
