9070fd4d4c64223d4d70c9de291fb7972db9f4ad
IceMage144 <jgabriel.basi@hotmail.com>
2018-11-21 21:22:46 -0200

    Refactor code and add types to function arguments

--- geocomp/common/binary_search_tree.py
+++ geocomp/common/binary_search_tree.py
@@ -2,13 +2,13 @@ class Node:
     RED = True
     BLACK = False
 
-    def __init__(self, id, key, color = RED):
+    def __init__(self, id: int, key, color: bool = RED):
         self.color = color
         self.key = key
         self.left = self.right = self.parent = NilNode.instance()
         self.id = id
 
-    def __str__(self, level = 0, indent = "   "):
+    def __str__(self, level: int = 0, indent: str = "   ") -> str:
         left_id = "Nil" if not self.left else self.left.id
         right_id = "Nil" if not self.right else self.right.id
         parent_id = "Nil" if not self.parent else self.parent.id
@@ -20,7 +20,7 @@ class Node:
     def __bool__(self):
         return True
 
-    def is_red(self):
+    def is_red(self) -> bool:
         return self.color == Node.RED
 
 
@@ -44,7 +44,7 @@ class NilNode(Node):
     def __bool__(self):
         return False
 
-    def is_red(self):
+    def is_red(self) -> bool:
         return False
 
 class BinarySearchTree:
@@ -53,7 +53,7 @@ class BinarySearchTree:
         self.size = 0
         self.control = {}
 
-    def __str__helper(self, node, level: int = 0, indent: str = "   "):
+    def __str__helper(self, node: Node, level: int = 0, indent: str = "   "):
         s = level * indent + str(node)
         if node.left:
             s += "\n" + self.__str__helper(node.left, level + 1, indent)
@@ -61,24 +61,24 @@ class BinarySearchTree:
             s += "\n" + self.__str__helper(node.right, level + 1, indent)
         return s
 
-    def __str__(self, indent="  "):
+    def __str__(self, indent: str = "  "):
         if not self.root:
             return "(root.size = 0, balanced = True)\n(Empty tree)"
-        res, err = self.is_rbt()
+        res, _ = self.is_rbt()
         return f"(root.size = {self.size}, RBT = {res})\n" + \
-                self.__str__helper(self.root, indent=indent)
+               self.__str__helper(self.root, indent=indent)
 
     def is_empty(self) -> bool:
         return bool(self.root)
 
     def insert(self, id: int, key):
-        x = Node(id, key)
-        self.control[x.id] = x
-        self.root = self.__insert(self.root, x)
+        new_node = Node(id, key)
+        self.control[new_node.id] = new_node
+        self.root = self.__insert(self.root, new_node)
         self.root.parent = NilNode.instance()
-        self.__insert_balance(x)
-        # self.root.color = Node.BLACK
+        self.__insert_balance(new_node)
         self.size += 1
+
         res, err = self.is_rbt()
         if not res:
             print(str(self))
@@ -95,96 +95,78 @@ class BinarySearchTree:
             act_node.right = self.__insert(act_node.right, new_node)
             act_node.right.parent = act_node
 
-        # if act_node.right.is_red() and not act_node.left.is_red():
-        #     act_node = self.__rotate_left(act_node)
-        # if act_node.left.is_red() and act_node.left.left.is_red():
-        #     act_node = self.__rotate_right(act_node)
-        # if act_node.left.is_red() and act_node.right.is_red():
-        #     self.__flip_colors(act_node)
-
         return act_node
 
-    def __insert_balance(self, node):
-        node.color = Node.RED
+    def __insert_balance(self, node: Node):
         while node != self.root and node.parent.is_red():
-            if node.parent == node.parent.parent.left:
-                uncle = node.parent.parent.right
+            parent = node.parent
+            if parent == parent.parent.left:
+                uncle = parent.parent.right
                 if uncle and uncle.is_red():
-                    # node.parent.color = Node.BLACK
-                    # uncle.color = Node.BLACK
-                    # node.parent.parent.color = Node.RED
-                    node = node.parent.parent
+                    node = parent.parent
                     self.__flip_colors(node)
                 else:
-                    if node == node.parent.right:
-                        node = node.parent
+                    if node == parent.right:
+                        node = parent
                         self.__rotate_left(node)
-                    # node.parent.color = Node.BLACK
-                    # node.parent.parent.color = Node.RED
                     self.__rotate_right(node.parent.parent)
             else:
-                uncle = node.parent.parent.left
+                uncle = parent.parent.left
                 if uncle and uncle.is_red():
-                    # node.parent.color = Node.BLACK
-                    # uncle.color = Node.BLACK
-                    # node.parent.parent.color = Node.RED
-                    node = node.parent.parent
+                    node = parent.parent
                     self.__flip_colors(node)
                 else:
-                    if node == node.parent.left:
-                        node = node.parent
+                    if node == parent.left:
+                        node = parent
                         self.__rotate_right(node)
-                    # node.parent.color = Node.BLACK
-                    # node.parent.parent.color = Node.RED
                     self.__rotate_left(node.parent.parent)
         self.root.color = Node.BLACK
 
-    def delete(self, id: int):
+    def delete(self, id: int) -> Node:
         if not (id in self.control):
             return
 
-        z = self.control[id]
+        old = self.control[id]
         del self.control[id]
-        if not z.left or not z.right:
-            y = z
-        else:
-            y = self.successor(z)
-            z.key = y.key
-            z.id = y.id
-            self.control[y.id] = z
 
-        x = y.right if not y.left else y.left
-        x.parent = y.parent
-        if y == y.parent.left:
-            y.parent.left = x
-        elif y == y.parent.right:
-            y.parent.right = x
-        else:
-            self.root = x
+        if old.left and old.right:
+            new_old = self.__successor(old)
+            old.key = new_old.key
+            old.id = new_old.id
+            self.control[old.id] = old
+            old = new_old
 
-        if not y.is_red():
-            if x.is_red():
-                x.color = Node.BLACK
+        child = old.right if not old.left else old.left
+        child.parent = old.parent
+        if old == old.parent.left:
+            old.parent.left = child
+        elif old == old.parent.right:
+            old.parent.right = child
+        else:
+            self.root = child
+
+        if not old.is_red():
+            if child.is_red():
+                child.color = Node.BLACK
             else:
-                self.__balance(x)
+                self.__delete_balance(child)
 
         self.size -= 1
+
         res, err = self.is_rbt()
         if not res:
             print(str(self))
             raise Exception(err)
-        return y
+        return old
 
-    def __balance(self, node):
+    def __delete_balance(self, node):
         if self.root == node or node.is_red():
-            # CASE 1/4
             node.color = Node.BLACK
             return
         left = (node == node.parent.left)
         parent = node.parent
         sibling = node.parent.right if left else node.parent.left
         if sibling.is_red():
-            # CASE 2
             if left:
                 self.__rotate_left(parent)
                 sibling = parent.right
@@ -192,28 +174,21 @@ class BinarySearchTree:
                 self.__rotate_right(parent)
                 sibling = parent.left
         if not sibling.right.is_red() and not sibling.left.is_red():
-            # CASE 3
             sibling.color = Node.RED
-            self.__balance(parent)
+            self.__delete_balance(parent)
         else:
             if left:
                 if not sibling.right.is_red():
-                    # CASE 5 left
                     self.__rotate_right(sibling)
                     sibling = parent.right
-                # CASE 6 left
-                parent = self.__rotate_left(parent)
-                parent.left.color = Node.BLACK
-                parent.right.color = Node.BLACK
+                self.__rotate_left(parent)
             else:
                 if not sibling.left.is_red():
-                    # CASE 5 right
                     self.__rotate_left(sibling)
                     sibling = parent.left
-                # CASE 6 right
-                parent = self.__rotate_right(parent)
-                parent.left.color = Node.BLACK
-                parent.right.color = Node.BLACK
+                self.__rotate_right(parent)
+            sibling.left.color = Node.BLACK
+            sibling.right.color = Node.BLACK
 
     def minimum(self, node = None):
         if node is None: node = self.root
@@ -227,64 +202,52 @@ class BinarySearchTree:
             node = node.right
         return node
 
-    def successor(self, node):
+    def __successor(self, node: Node):
         if node.right:
             return self.minimum(node.right)
-        y = node.parent
-        while y and node == y.right:
-            node = y
-            y = y.parent
-        return y
+        parent = node.parent
+        while parent and node == parent.right:
+            node, parent = parent, parent.parent
+        return parent
 
-    def predecessor(self, node):
-        if node.left:
-            return self.maximum(node.left)
-        y = node.parent
-        while y and node == y.left:
-            node = y
-            y = y.parent
-        return y
-
-    def __rotate_right(self, node):
-        x = node.left
-        node.left = x.right
-        x.right = node
-        x.color = x.right.color
-        x.right.color = Node.RED
-        x.parent = node.parent
-        node.parent = x
+    def __rotate_right(self, node: Node):
+        child = node.left
+        node.left = child.right
+        child.right = node
+        child.color = child.right.color
+        child.right.color = Node.RED
+        child.parent = node.parent
+        node.parent = child
         if node.left: node.left.parent = node
-        if x.parent.left == node:
-            x.parent.left = x
-        elif x.parent.right == node:
-            x.parent.right = x
+        if child.parent.left == node:
+            child.parent.left = child
+        elif child.parent.right == node:
+            child.parent.right = child
         else:
-            self.root = x
-        return x
+            self.root = child
 
-    def __rotate_left(self, node):
-        x = node.right
-        node.right = x.left
-        x.left = node
-        x.color = x.left.color
-        x.left.color = Node.RED
-        x.parent = node.parent
-        node.parent = x
+    def __rotate_left(self, node: Node):
+        child = node.right
+        node.right = child.left
+        child.left = node
+        child.color = child.left.color
+        child.left.color = Node.RED
+        child.parent = node.parent
+        node.parent = child
         if node.right: node.right.parent = node
-        if x.parent.left == node:
-            x.parent.left = x
-        elif x.parent.right == node:
-            x.parent.right = x
+        if child.parent.left == node:
+            child.parent.left = child
+        elif child.parent.right == node:
+            child.parent.right = child
         else:
-            self.root = x
-        return x
+            self.root = child
 
-    def __flip_colors(self, node):
+    def __flip_colors(self, node: Node):
         node.color = not node.color
         node.left.color =  not node.left.color
         node.right.color =  not node.right.color
 
-    def is_rbt(self):
+    def is_rbt(self) -> tuple:
         if not self.root:
             return (True, "")
         node = self.root
@@ -293,7 +256,7 @@ class BinarySearchTree:
             node = node.right
             if not node.is_red():
                 final_blacks += 1
-        errors, ids = self.__is_rbt(self.root, 0, final_blacks, 0)
+        errors, ids = self.__is_rbt(self.root, 0, final_blacks)
         if errors == 0:
             return (True, "")
         elif errors == 1:
@@ -302,15 +265,15 @@ class BinarySearchTree:
             return (False, f"Tree has double red nodes on ids {ids}")
         return (False, f"Tree is not balanced and has double red nodes on ids {ids}")
 
-    def __is_rbt(self, node, blacks, final_blacks, level):
+    def __is_rbt(self, node, blacks: int, final_blacks: int):
         if not node.is_red():
             blacks += 1
         elif node.left.is_red() or node.right.is_red():
             return (2, [node.id])
         if not node:
             return (0, []) if blacks == final_blacks else (1, [-1])
-        lerr, lids = self.__is_rbt(node.left, blacks, final_blacks, level + 1)
-        rerr, rids =self.__is_rbt(node.right, blacks, final_blacks, level + 1)
+        lerr, lids = self.__is_rbt(node.left, blacks, final_blacks)
+        rerr, rids = self.__is_rbt(node.right, blacks, final_blacks)
         if lerr != 0 and rerr != 0:
             return (lerr ^ rerr, lids + rids)
         if lerr != 0:
